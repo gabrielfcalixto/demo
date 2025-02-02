9c1d982cb9ff346404f11cbb68a5249bfd989ad4
Gabriel Calixto <gfcdev@outlook.com.br>
2025-02-02 15:42:02 -0300

    first commit

---
+++ src/test/java/com/gfctech/project_manager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfctech.project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
