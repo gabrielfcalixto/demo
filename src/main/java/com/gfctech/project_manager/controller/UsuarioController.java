f44d3c7b4654a9aa0b85b41c0895bde9f9c8b50d
Gabriel Calixto <gfcdev@outlook.com.br>
2025-02-02 17:29:10 -0300

    feature: implementada a classe UsuarioController e métodos no UsuarioService para gerenciamento de usuários

--- src/main/java/com/gfctech/project_manager/controller/UsuarioController.java
+++ src/main/java/com/gfctech/project_manager/controller/UsuarioController.java
@@ -1,9 +1,49 @@
 package com.gfctech.project_manager.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class UsuarioController {
-    
+import com.gfctech.project_manager.service.UsuarioService;
 
+@RestController
+@RequestMapping(value = "/usuario")
+@CrossOrigin(origins = "http://localhost:4000")
+public class UsuarioController
+ {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<UsuarioDTO>ListarTodos()
+    {
+        return usuarioService.listarTodos();
+    }
+
+    @PostMapping
+    public void inserir(@RequestBody UsuarioDTO usuario)
+    {
+        usuarioService.inserir(usuario);
+    }
+
+    @PutMapping
+    public void alterar(@RequestBody UsuarioDTO usuario)
+    {
+        return usuarioService.alterar(usuario);
+    }
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id)
+    {
+        usuarioService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/gfctech/project_manager/service/UsuarioService.java
+++ src/main/java/com/gfctech/project_manager/service/UsuarioService.java
@@ -1,5 +1,66 @@
 package com.gfctech.project_manager.service;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.gfctech.project_manager.dto.UsuarioDTO;
+import com.gfctech.project_manager.entity.UsuarioEntity;
+import com.gfctech.project_manager.repository.UsuarioRepository;
+
+@Service
 public class UsuarioService {
+
+
+    @Autowired  
+	private UsuarioRepository usuarioRepository;
+	
+	// @Autowired
+	// private UsuarioVerificadorRepository usuarioVerificadorRepository;
+	
+	// @Autowired(required = true)
+	// private PasswordEncoder passwordEncoder;
+	
+	// @Autowired
+	// private EmailService emailService;
     
+    public List<UsuarioDTO> listarTodos(){
+		List<UsuarioEntity> usuarios = usuarioRepository.findAll();
+		return usuarios.stream() .map(UsuarioDTO::new).toList();
+	}
+	
+	public void inserir(UsuarioDTO usuario) {
+		UsuarioEntity usuarioEntity = new UsuarioEntity(usuario);
+		usuarioEntity.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		usuarioRepository.save(usuarioEntity);
+		
+
+	}
+	
+	public void inserirNovoUsuario(UsuarioDTO usuario) {
+		UsuarioEntity usuarioEntity = new UsuarioEntity(usuario);
+		// usuarioEntity. setSituacao(TipoSituacaoUsuario.PENDENTE); 
+		usuarioEntity.setId(null);
+		usuarioRepository.save(usuarioEntity);
+
+    }
+
+	public UsuarioDTO alterar(UsuarioDTO usuario) {
+		UsuarioEntity usuarioEntity = new UsuarioEntity(usuario);
+		return new UsuarioDTO(usuarioRepository.save(usuarioEntity));
+	}
+	
+	public void excluir(Long id) {
+		UsuarioEntity usuario = usuarioRepository.findById(id).get();
+		usuarioRepository.delete(usuario);
+	}
+	
+	public UsuarioDTO buscarPorIdDto(Long id) {
+		return new UsuarioDTO(usuarioRepository.findById(id).get());
+	}
 }
+
