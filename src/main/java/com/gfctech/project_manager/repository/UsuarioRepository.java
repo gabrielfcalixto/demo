8e827125f819aafe8977a27dd1c79670eaefa0c4
Gabriel Calixto <gfcdev@outlook.com.br>
2025-02-02 17:17:58 -0300

    feature: adicionado repositório UsuarioRepository com método de busca por login

--- src/main/java/com/gfctech/project_manager/repository/UsuarioRepository.java
+++ src/main/java/com/gfctech/project_manager/repository/UsuarioRepository.java
@@ -1,5 +1,12 @@
 package com.gfctech.project_manager.repository;
 
-public class UsuarioRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gfctech.project_manager.entity.UsuarioEntity;
+
+public interface UsuarioRepository  extends JpaRepository<UsuarioEntity, Long> {
+    Optional<UsuarioEntity> findByLogin(String login);
     
 }
